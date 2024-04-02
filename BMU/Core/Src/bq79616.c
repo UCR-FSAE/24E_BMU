f103c6f0629e8f8d1a6fd1c94a1d3b13da8b53b6
SRaionido <stevenryanleonido@gmail.com>
2024-04-01 20:12:51 -0700

    Saved and added updated TODO

--- BMU/Core/Src/bq79616.c
+++ BMU/Core/Src/bq79616.c
@@ -18,7 +18,7 @@
     - Replace all instances of SPI transmit/transmitreceive/receive with STM HAL versions
     - Replace all instances of delayms with STM HAL versions
     - Make new version of delayus
-    - Change all uints to match STM versions
+    - Change all uints to match STM versions DONE
     - Replace all instances of gioGetBit with STM HAL versions/figure out how pin works with BQ79600 datasheet
     - Get rid of uneccesary functions, variables and includes from TI microcontroller
     - Remake wake functions
@@ -27,24 +27,24 @@
 #include <bq79616.h>
 #include <bq79600.h>
 #include "string.h"
-#include "sci.h"
-#include "spi.h"
-#include "rti.h"
-#include "gio.h"
+#include "sci.h" // FROM SL: TI functions used in printConsole function, may take out
+#include "spi.h" // FROM SL: TI functions used in SPI functions, may take out
+#include "rti.h" // FROM SL: TI function used in delay functions, may take out
+#include "gio.h" // FROM SL: TI functions used in wake functions, may take out
 #include "datatypes.h"
 #include "stdarg.h"
 
 // GLOBAL VARIABLES (use these to avoid stack overflows by creating too many function variables)
 // avoid creating variables/arrays in functions, or you will run out of stack space quickly
-uint16 response_frame2[(MAXBYTES + 6) * TOTALBOARDS]; // response frame to be used by every read
-uint16 fault_frame[39 * TOTALBOARDS];                 // hold fault frame if faults are printed
+uint16_t response_frame2[(MAXBYTES + 6) * TOTALBOARDS]; // response frame to be used by every read
+uint16_t fault_frame[39 * TOTALBOARDS];                 // hold fault frame if faults are printed
 int currentBoard = 0;
 int currentCell = 0;
 BYTE bReturn = 0;
 int bRes = 0;
 int count = 10000;
 BYTE bBuf[8];
-uint8 pFrame[64];
+uint8_t pFrame[64];
 static volatile unsigned int delayval = 0; // for delayms and delayus functions
 extern int UART_RX_RDY;
 extern int RTI_TIMEOUT;
@@ -53,22 +53,22 @@ int baseCommunicating = 0;
 int otpPass = 0;
 BYTE *currCRC;
 int crc_i = 0;
-uint16 wCRC2 = 0xFFFF;
+uint16_t wCRC2 = 0xFFFF;
 int crc16_i = 0;
 
-uint16 autoaddr_response_frame[(1 + 6) * TOTALBOARDS]; // response frame for auto-addressing sequence
+uint16_t autoaddr_response_frame[(1 + 6) * TOTALBOARDS]; // response frame for auto-addressing sequence
 int numReads = 0;
 int channel = 0;
 
 // SpiWriteFrame
-uint16 spiBuf[8];
-uint16 spiFrame[64];
+uint16_t spiBuf[8];
+uint16_t spiFrame[64];
 int spiPktLen = 0;
-uint16 *spiPBuf = spiFrame;
-uint16 spiWCRC;
+uint16_t *spiPBuf = spiFrame;
+uint16_t spiWCRC;
 
 // SpiReadReg
-uint16 spiReturn = 0;
+uint16_t spiReturn = 0;
 int M = 0; // expected total response bytes
 int i = 0; // number of groups of 128 bytes
 int K = 0; // number of bytes remaining in the last group of 128
@@ -81,7 +81,7 @@ int K = 0; // number of bytes remaining in the last group of 128
 
 void SpiWake79600(void)
 {
-    spiREG3->PC0 &= ~(uint32)((uint32)(1U << 10U | 1U << 1U)); // disable MOSI and nCS - now GPIO
+    spiREG3->PC0 &= ~(uint32_t)((uint32_t)(1U << 10U | 1U << 1U)); // disable MOSI and nCS - now GPIO
     gioToggleBit(spiPORT3, 1U);
     delayus(0.5);
     gioSetBit(spiPORT3, 10U, 0);
@@ -89,11 +89,11 @@ void SpiWake79600(void)
     gioSetBit(spiPORT3, 10U, 1);
     delayus(0.5);
     gioToggleBit(spiPORT3, 1U);
-    spiREG3->PC0 |= (uint32)((uint32)(1U << 10U | 1U << 1U)); // re-enable MOSI and nCS - now SPI
+    spiREG3->PC0 |= (uint32_t)((uint32_t)(1U << 10U | 1U << 1U)); // re-enable MOSI and nCS - now SPI
 }
 void SpiSD79600(void)
 {
-    spiREG3->PC0 &= ~(uint32)((uint32)(1U << 10U | 1U << 1U)); // disable MOSI and nCS - now GPIO
+    spiREG3->PC0 &= ~(uint32_t)((uint32_t)(1U << 10U | 1U << 1U)); // disable MOSI and nCS - now GPIO
     gioToggleBit(spiPORT3, 1U);
     delayus(0.5);
     gioSetBit(spiPORT3, 10U, 0);
@@ -101,11 +101,11 @@ void SpiSD79600(void)
     gioSetBit(spiPORT3, 10U, 1);
     delayus(0.5);
     gioToggleBit(spiPORT3, 1U);
-    spiREG3->PC0 |= (uint32)((uint32)(1U << 10U | 1U << 1U)); // re-enable MOSI and nCS - now SPI
+    spiREG3->PC0 |= (uint32_t)((uint32_t)(1U << 10U | 1U << 1U)); // re-enable MOSI and nCS - now SPI
 }
 void SpiStA79600(void)
 {
-    spiREG3->PC0 &= ~(uint32)((uint32)(1U << 10U | 1U << 1U)); // disable MOSI and nCS - now GPIO
+    spiREG3->PC0 &= ~(uint32_t)((uint32_t)(1U << 10U | 1U << 1U)); // disable MOSI and nCS - now GPIO
     gioToggleBit(spiPORT3, 1U);
     delayus(0.5);
     gioSetBit(spiPORT3, 10U, 0);
@@ -113,7 +113,7 @@ void SpiStA79600(void)
     gioSetBit(spiPORT3, 10U, 1);
     delayus(0.5);
     gioToggleBit(spiPORT3, 1U);
-    spiREG3->PC0 |= (uint32)((uint32)(1U << 10U | 1U << 1U)); // re-enable MOSI and nCS - now SPI
+    spiREG3->PC0 |= (uint32_t)((uint32_t)(1U << 10U | 1U << 1U)); // re-enable MOSI and nCS - now SPI
 }
 
 void SpiCommClear79600(void)
@@ -183,7 +183,7 @@ void SpiAutoAddress()
 //************************
 // WRITE AND READ FUNCTIONS
 //************************
-int SpiWriteReg(BYTE bID, uint16 wAddr, uint64 dwData, BYTE bLen, BYTE bWriteType)
+int SpiWriteReg(BYTE bID, uint16_t wAddr, uint64_t dwData, BYTE bLen, BYTE bWriteType)
 {
     // device address, register start address, data bytes, data length, write type (single, broadcast, stack)
     bRes = 0;
@@ -258,7 +258,7 @@ int SpiWriteReg(BYTE bID, uint16 wAddr, uint64 dwData, BYTE bLen, BYTE bWriteTyp
     return bRes;
 }
 
-int SpiWriteFrame(uint16 bID, uint16 wAddr, uint16 *pData, uint16 bLen, uint8 bWriteType)
+int SpiWriteFrame(uint16_t bID, uint16_t wAddr, uint16_t *pData, uint16_t bLen, uint8_t bWriteType)
 {
     spiPktLen = 0;
     spiPBuf = spiFrame;
@@ -287,7 +287,7 @@ int SpiWriteFrame(uint16 bID, uint16 wAddr, uint16 *pData, uint16 bLen, uint8 bW
 }
 
 // GENERATE READ COMMAND FRAME AND THEN WAIT FOR RESPONSE DATA (INTERRUPT MODE FOR SCIRX)
-int SpiReadReg(BYTE bID, uint16 wAddr, uint16 *pData, BYTE bLen, uint32 dwTimeOut, BYTE bWriteType)
+int SpiReadReg(BYTE bID, uint16_t wAddr, uint16_t *pData, BYTE bLen, uint32_t dwTimeOut, BYTE bWriteType)
 {
     // device address, register start address, byte frame pointer to store data, data length, read type (single, broadcast, stack)
 
@@ -301,7 +301,7 @@ int SpiReadReg(BYTE bID, uint16 wAddr, uint16 *pData, BYTE bLen, uint32 dwTimeOu
     SpiWriteFrame(bID, wAddr, &spiReturn, 1, bWriteType); // send the read request command frame    // Replace all instances of this with STM HAL ver
     delayus(5 * TOTALBOARDS);                             // wait propagation time for each board
 
-    uint16 *movingPointer = pData;
+    uint16_t *movingPointer = pData;
 
     // prepare the correct number of bytes for the device to read
     if (bWriteType == FRMWRT_SGL_R)
@@ -379,44 +379,44 @@ int SpiReadReg(BYTE bID, uint16 wAddr, uint16 *pData, BYTE bLen, uint32 dwTimeOu
 
 // CRC16 TABLE
 // ITU_T polynomial: x^16 + x^15 + x^2 + 1
-const uint16 crc16_table[256] = {0x0000, 0xC0C1, 0xC181, 0x0140, 0xC301,
-                                 0x03C0, 0x0280, 0xC241, 0xC601, 0x06C0, 0x0780, 0xC741, 0x0500, 0xC5C1,
-                                 0xC481, 0x0440, 0xCC01, 0x0CC0, 0x0D80, 0xCD41, 0x0F00, 0xCFC1, 0xCE81,
-                                 0x0E40, 0x0A00, 0xCAC1, 0xCB81, 0x0B40, 0xC901, 0x09C0, 0x0880, 0xC841,
-                                 0xD801, 0x18C0, 0x1980, 0xD941, 0x1B00, 0xDBC1, 0xDA81, 0x1A40, 0x1E00,
-                                 0xDEC1, 0xDF81, 0x1F40, 0xDD01, 0x1DC0, 0x1C80, 0xDC41, 0x1400, 0xD4C1,
-                                 0xD581, 0x1540, 0xD701, 0x17C0, 0x1680, 0xD641, 0xD201, 0x12C0, 0x1380,
-                                 0xD341, 0x1100, 0xD1C1, 0xD081, 0x1040, 0xF001, 0x30C0, 0x3180, 0xF141,
-                                 0x3300, 0xF3C1, 0xF281, 0x3240, 0x3600, 0xF6C1, 0xF781, 0x3740, 0xF501,
-                                 0x35C0, 0x3480, 0xF441, 0x3C00, 0xFCC1, 0xFD81, 0x3D40, 0xFF01, 0x3FC0,
-                                 0x3E80, 0xFE41, 0xFA01, 0x3AC0, 0x3B80, 0xFB41, 0x3900, 0xF9C1, 0xF881,
-                                 0x3840, 0x2800, 0xE8C1, 0xE981, 0x2940, 0xEB01, 0x2BC0, 0x2A80, 0xEA41,
-                                 0xEE01, 0x2EC0, 0x2F80, 0xEF41, 0x2D00, 0xEDC1, 0xEC81, 0x2C40, 0xE401,
-                                 0x24C0, 0x2580, 0xE541, 0x2700, 0xE7C1, 0xE681, 0x2640, 0x2200, 0xE2C1,
-                                 0xE381, 0x2340, 0xE101, 0x21C0, 0x2080, 0xE041, 0xA001, 0x60C0, 0x6180,
-                                 0xA141, 0x6300, 0xA3C1, 0xA281, 0x6240, 0x6600, 0xA6C1, 0xA781, 0x6740,
-                                 0xA501, 0x65C0, 0x6480, 0xA441, 0x6C00, 0xACC1, 0xAD81, 0x6D40, 0xAF01,
-                                 0x6FC0, 0x6E80, 0xAE41, 0xAA01, 0x6AC0, 0x6B80, 0xAB41, 0x6900, 0xA9C1,
-                                 0xA881, 0x6840, 0x7800, 0xB8C1, 0xB981, 0x7940, 0xBB01, 0x7BC0, 0x7A80,
-                                 0xBA41, 0xBE01, 0x7EC0, 0x7F80, 0xBF41, 0x7D00, 0xBDC1, 0xBC81, 0x7C40,
-                                 0xB401, 0x74C0, 0x7580, 0xB541, 0x7700, 0xB7C1, 0xB681, 0x7640, 0x7200,
-                                 0xB2C1, 0xB381, 0x7340, 0xB101, 0x71C0, 0x7080, 0xB041, 0x5000, 0x90C1,
-                                 0x9181, 0x5140, 0x9301, 0x53C0, 0x5280, 0x9241, 0x9601, 0x56C0, 0x5780,
-                                 0x9741, 0x5500, 0x95C1, 0x9481, 0x5440, 0x9C01, 0x5CC0, 0x5D80, 0x9D41,
-                                 0x5F00, 0x9FC1, 0x9E81, 0x5E40, 0x5A00, 0x9AC1, 0x9B81, 0x5B40, 0x9901,
-                                 0x59C0, 0x5880, 0x9841, 0x8801, 0x48C0, 0x4980, 0x8941, 0x4B00, 0x8BC1,
-                                 0x8A81, 0x4A40, 0x4E00, 0x8EC1, 0x8F81, 0x4F40, 0x8D01, 0x4DC0, 0x4C80,
-                                 0x8C41, 0x4400, 0x84C1, 0x8581, 0x4540, 0x8701, 0x47C0, 0x4680, 0x8641,
-                                 0x8201, 0x42C0, 0x4380, 0x8341, 0x4100, 0x81C1, 0x8081, 0x4040};
+const uint16_t crc16_table[256] = {0x0000, 0xC0C1, 0xC181, 0x0140, 0xC301,
+                                   0x03C0, 0x0280, 0xC241, 0xC601, 0x06C0, 0x0780, 0xC741, 0x0500, 0xC5C1,
+                                   0xC481, 0x0440, 0xCC01, 0x0CC0, 0x0D80, 0xCD41, 0x0F00, 0xCFC1, 0xCE81,
+                                   0x0E40, 0x0A00, 0xCAC1, 0xCB81, 0x0B40, 0xC901, 0x09C0, 0x0880, 0xC841,
+                                   0xD801, 0x18C0, 0x1980, 0xD941, 0x1B00, 0xDBC1, 0xDA81, 0x1A40, 0x1E00,
+                                   0xDEC1, 0xDF81, 0x1F40, 0xDD01, 0x1DC0, 0x1C80, 0xDC41, 0x1400, 0xD4C1,
+                                   0xD581, 0x1540, 0xD701, 0x17C0, 0x1680, 0xD641, 0xD201, 0x12C0, 0x1380,
+                                   0xD341, 0x1100, 0xD1C1, 0xD081, 0x1040, 0xF001, 0x30C0, 0x3180, 0xF141,
+                                   0x3300, 0xF3C1, 0xF281, 0x3240, 0x3600, 0xF6C1, 0xF781, 0x3740, 0xF501,
+                                   0x35C0, 0x3480, 0xF441, 0x3C00, 0xFCC1, 0xFD81, 0x3D40, 0xFF01, 0x3FC0,
+                                   0x3E80, 0xFE41, 0xFA01, 0x3AC0, 0x3B80, 0xFB41, 0x3900, 0xF9C1, 0xF881,
+                                   0x3840, 0x2800, 0xE8C1, 0xE981, 0x2940, 0xEB01, 0x2BC0, 0x2A80, 0xEA41,
+                                   0xEE01, 0x2EC0, 0x2F80, 0xEF41, 0x2D00, 0xEDC1, 0xEC81, 0x2C40, 0xE401,
+                                   0x24C0, 0x2580, 0xE541, 0x2700, 0xE7C1, 0xE681, 0x2640, 0x2200, 0xE2C1,
+                                   0xE381, 0x2340, 0xE101, 0x21C0, 0x2080, 0xE041, 0xA001, 0x60C0, 0x6180,
+                                   0xA141, 0x6300, 0xA3C1, 0xA281, 0x6240, 0x6600, 0xA6C1, 0xA781, 0x6740,
+                                   0xA501, 0x65C0, 0x6480, 0xA441, 0x6C00, 0xACC1, 0xAD81, 0x6D40, 0xAF01,
+                                   0x6FC0, 0x6E80, 0xAE41, 0xAA01, 0x6AC0, 0x6B80, 0xAB41, 0x6900, 0xA9C1,
+                                   0xA881, 0x6840, 0x7800, 0xB8C1, 0xB981, 0x7940, 0xBB01, 0x7BC0, 0x7A80,
+                                   0xBA41, 0xBE01, 0x7EC0, 0x7F80, 0xBF41, 0x7D00, 0xBDC1, 0xBC81, 0x7C40,
+                                   0xB401, 0x74C0, 0x7580, 0xB541, 0x7700, 0xB7C1, 0xB681, 0x7640, 0x7200,
+                                   0xB2C1, 0xB381, 0x7340, 0xB101, 0x71C0, 0x7080, 0xB041, 0x5000, 0x90C1,
+                                   0x9181, 0x5140, 0x9301, 0x53C0, 0x5280, 0x9241, 0x9601, 0x56C0, 0x5780,
+                                   0x9741, 0x5500, 0x95C1, 0x9481, 0x5440, 0x9C01, 0x5CC0, 0x5D80, 0x9D41,
+                                   0x5F00, 0x9FC1, 0x9E81, 0x5E40, 0x5A00, 0x9AC1, 0x9B81, 0x5B40, 0x9901,
+                                   0x59C0, 0x5880, 0x9841, 0x8801, 0x48C0, 0x4980, 0x8941, 0x4B00, 0x8BC1,
+                                   0x8A81, 0x4A40, 0x4E00, 0x8EC1, 0x8F81, 0x4F40, 0x8D01, 0x4DC0, 0x4C80,
+                                   0x8C41, 0x4400, 0x84C1, 0x8581, 0x4540, 0x8701, 0x47C0, 0x4680, 0x8641,
+                                   0x8201, 0x42C0, 0x4380, 0x8341, 0x4100, 0x81C1, 0x8081, 0x4040};
 
-uint32 SpiCRC16(uint16 *pBuf, int nLen)
+uint32_t SpiCRC16(uint16_t *pBuf, int nLen)
 {
-    uint32 wCRC = 0xFFFF;
+    uint32_t wCRC = 0xFFFF;
     int i;
 
     for (i = 0; i < nLen; i++)
     {
-        wCRC ^= (uint16)(*pBuf++) & 0x00FF;
+        wCRC ^= (uint16_t)(*pBuf++) & 0x00FF;
         wCRC = crc16_table[wCRC & 0x00FF] ^ (wCRC >> 8);
     }
 
@@ -437,7 +437,7 @@ void SpiDisableTimeout_600_616(void)
     SpiWriteReg(0, COMM_CTRL, 0x00, 1, FRMWRT_STK_W);
 }
 
-float Complement(uint16 rawData, float multiplier)
+float Complement(uint16_t rawData, float multiplier)
 {
     return -1 * (~rawData + 1) * multiplier;
 }
@@ -450,7 +450,7 @@ BOOL GetFaultStat()
     return 1;
 }
 
-void delayus(uint16 us) // Make new version of this
+void delayus(uint16_t us) // Make new version of this
 {
     if (us == 0)
         return;
@@ -488,7 +488,7 @@ void delayus(uint16 us) // Make new version of this
     }
 }
 
-void delayms(uint16 ms) // Make new version of this
+void delayms(uint16_t ms) // Make new version of this
 {
     if (ms == 0)
         return;
@@ -507,7 +507,7 @@ void delayms(uint16 ms) // Make new version of this
     }
 }
 
-uint16_t volt2Byte(float volt)
+uint16_t volt2Byte(float volt) // FROM SL: Take out? May be uneccesary
 {
     return (uint16_t) ~((int16_t)((-volt / 0.00019073) - 1.0));
 }
